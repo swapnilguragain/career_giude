2872d968147f97217a775de690c0e6f1e7449a65
swapnilguragain <guragainswapnil@gmail.com>
2017-06-28 11:30:36 +0545

    catalogues changed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Interior Design' });
+});
+
+router.get('/contacts', function(req, res, next) {
+  res.render('contacts', { title: 'Contacts' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Contacts' });
+});
+
+router.get('/catalogue', function(req, res, next) {
+  res.render('catalogue', { title: 'Contacts' });
 });
 
 module.exports = router;
